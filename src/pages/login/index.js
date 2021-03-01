faa296c353f51f56e8407e188269a4eb39eb3be6
danail2003 <ddobrev21@gmail.com>
2021-03-01 20:07:14 +0200

    feat: some little changes to login page

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -4,13 +4,29 @@ import Title from '../../components/title/index';
 import PageLayout from '../../components/page-layout/index';
 
 const Login = () => {
-    const[username, setUsername] = useState('');
+    const[email, setEmail] = useState('');
     const[password, setPassword] = useState('');
+    const apiKey = 'AIzaSyDYNWoeX46SOv9246LONV9BWFY8JGEoI_0';
 
-    const Submit = (e) => {
+    const Submit = async (e) => {
         e.preventDefault();
 
-        
+        if(!email || !password || !email.includes('@')) {
+            return;
+        }
+
+        const request = await fetch(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${apiKey}`, {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify({
+                email,
+                password,
+            }),
+        })
+        .then(res => res.json())
+        .then(data => console.log(data))
     };
 
     return(
@@ -18,14 +34,14 @@ const Login = () => {
             <Title />
             <form onSubmit={Submit}>
                 <div>
-                    <label htmlFor='username'>Username</label>
-                    <input id='username' type='text' onChange={(e) => setUsername(e.target.value)} value={username} />
+                    <label htmlFor='email'>Email</label>
+                    <input id='email' type='text' onChange={(e) => setEmail(e.target.value)} value={email} />
                 </div>
                 <div>
                     <label htmlFor='password'>Password</label>
                     <input id='password' type='password' onChange={(e) => setPassword(e.target.value)} value={password} />
                 </div>
-                <button type='submit'>Register</button>
+                <button type='submit'>Login</button>
             </form>
         </PageLayout>
     );
