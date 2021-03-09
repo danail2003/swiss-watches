38df58bc039073173892ff397dfd12d7eaa9385a
danail2003 <ddobrev21@gmail.com>
2021-03-09 21:01:27 +0200

    feat: styling components

--- src/pages/home-page/index.js
+++ src/pages/home-page/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import styles from './home.module.css';
 import PageLayout from '../../components/page-layout/index';
 import Title from '../../components/title/index';
 
 const Home = () => {
     return(
         <PageLayout>
-            <Title title='All watches' />
+            <Title title='Everything is here' className={styles.heading} />
             <main>
-                <video autoPlay playsInline muted loop>
+                <video autoPlay playsInline muted loop className={styles.video}>
                     <source src='https://www.tissotwatches.com/media/Videos/video_homepage_prx.mp4' type='video/mp4' />
                 </video>
             </main>
