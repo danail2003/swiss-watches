f8563904e5b1ccce95ae673890ccc46d23502442
danail2003 <ddobrev21@gmail.com>
2021-03-20 17:48:04 +0200

    feat: styling and removed home-page video

--- src/pages/home-page/index.js
+++ src/pages/home-page/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import styles from './home.module.css';
 import PageLayout from '../../components/page-layout/index';
+import image from '../../images/swiss-watches.jpg';
 
 const Home = () => {
-    return(
+    return (
         <PageLayout>
             <main>
-                <video autoPlay playsInline muted loop className={styles.video}>
-                    <source src='https://www.tissotwatches.com/media/Videos/video_homepage_prx.mp4' type='video/mp4' />
-                </video>
+                <h1 className={styles.header}>Best watches site ever</h1>
+                <img src={image} alt='swiss-watches' className={styles.image}></img>
             </main>
         </PageLayout>
     );
